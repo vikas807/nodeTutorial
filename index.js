19049d7fd4bdc0b452bce7de4251102b6ca246a3
technicalguftgu <vpal807@gmail.com>
2021-12-31 20:01:29 +0530

    second commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 const port = 3000
 
 // this is api
-app.get('/', (req, res) => res.send('Hello Jenkins world'))
+app.get('/', (req, res) => res.send('Hello Jenkins World'))
 
 // bye world
 app.get('/bye', (req, res) => res.send('Bye World!'))
